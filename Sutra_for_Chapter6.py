2bd82a7b7c757ec03e30bc48a93da0f892d9f59d
nagata4139 <nagata4139@gmail.com>
2020-08-31 00:23:59 +0900

    更新

--- Sutra_for_Chapter6.py
+++ Sutra_for_Chapter6.py
@@ -50,3 +50,168 @@ ff
 # http://tinyurl.com/zvm9gng
 
 "Sawyer" * 3
+
+# P92
+
+http://tinyurl.com/hhancz6
+
+"We hold these truths...".upper()
+
+# http://tinyurl.com/zkz48u5
+
+"SO IT GOES.".lower()
+
+# http://tinyurl.com/jp5hexn
+
+"four score and...".capitalize()
+
+# P93
+# http://tinyurl.com/juvguy8
+
+"こんにちは、{}".format("ウィリアム・フォークナー")
+
+# http://tinyurl.com/zcpt9se
+
+name = "ウィリアム・フォークナー"
+"こんにちは、{}".format(name)
+
+# http://tinyurl.com/z6t6d8n
+
+author = "ウィリアム・フォークナー"
+year_born = "1897"
+
+"{} は {} 年に生まれました。".format(author, year_born)
+
+# http://tinyurl.com/gnrdsj9
+
+what = input("何が:")
+when = input("いつ:")
+where = input("どこで:")
+do = input("どうした:")
+r = "{}は{}に{}で{}。".format(what, when, where, do)
+print(r)
+
+# P94
+# http://tinyurl.com/he8u28o
+
+"水たまりを飛び越えたんだ。3メートルもあったんだぜ！".split("。")
+
+# http://tinyurl.com/h2pjkso
+
+first_three = "abc"
+result = "+".join(first_three)
+result
+
+# P95
+# http://tinyurl.com/z49e3up
+
+words = ["The", "fox", "jumped",
+    "over", "the", "fence", "."]
+one = "".join(words)
+one
+
+# http://tinyurl.com/h4qq5oy
+
+words = ["The", "fox", "jumped",
+    "over", "the", "fence", "."]
+one = " ".join(words)
+one
+
+# http://tinyurl.com/jfndhgx
+
+s = "   The   "
+s = s.strip()
+s
+
+# P96
+# http://tinyurl.com/zha4uwo
+
+equ = "All animals are equal."
+equ = equ.replace(a, "@")
+print(equ)
+
+# http://tinyurl.com/hzc6asc
+
+"animals".index("m")
+
+# http://tinyurl.com/jmtx984
+
+"animals".index("m")
+
+#http://tinyurl.com/jmtc984
+
+"animals".index("z")
+
+# P97
+# http://tinyurl.com/z16q4fd
+
+try:
+    "animals".index("z")
+except
+    print("Not Found.")
+
+# http://tinyurl.com/hsnygwz
+
+"Cat" in "Cat in the hat."
+
+# http://tinyurl.com/z9b3e97
+
+"Bat" in "Cat in the hat."
+
+# http://tinyurl.com/jz8sygd
+
+"Potter" not in "Harry"
+
+# http://tynyurl.com/zj6hc4r
+
+# このコードは動作しません
+
+"彼女は"そうだね"と言った"
+
+# http://tinyurl.com/jdsrr7e
+
+"彼女は \"そうだね\" と言った"
+
+# http:/tinyurl.com/zr7o7d7
+
+'彼女は \'そうだね\' と言った'
+
+# P99
+# http://tinyurl.com/hoef63o
+
+"彼女は 'そうだね' と言った"
+
+# http://tinyurl.com/zkgfawo
+
+'彼女は "そうだね" と言った'
+
+# http://tinyurl.com/zyrhaeg
+
+print("line1\nline2\nline3")
+
+# http://tinyurl.com/h2eqj2a
+
+fict = ["トルストイ", "カミュ", "オーウェル",
+        "ハクスリー", "オースティン"]
+fict[0:3]
+
+# http://tinyurl.com/hug9euj
+
+ivan = "死の代わりにひとつの光があった。"
+ivan[0:6]
+ivan[6:16]
+
+# http://tinyurl.com/judcpx4
+
+ivan = "死の代わりにひとつの光があった。"
+ivan[:6]
+
+# http://tinyurl.com/zqoscn4
+
+ivan = "死の代わりにひとつの光があった。"
+ivan[6:]
+
+# http://tinyurl.com/zqvuqoc
+
+ivan = "死の代わりにひとつの光があった。"
+ivan[:]
