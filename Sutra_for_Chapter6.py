ecac5df8e96feb4ecdd81c45d455c7d2418bb37a
nagata4139 <nagata4139@gmail.com>
2020-08-30 23:39:47 +0900

    更新

---
+++ Sutra_for_Chapter6.py
@@ -0,0 +1,52 @@
+# P88
+# http://tinyurl.com/h59ygda
+
+""" line one
+    line two
+    line three
+"""
+
+# http://tinyurl.com/zqqc2jw
+
+author = "Kafka"
+author[0]
+author[1]
+author[2]
+author[3]
+author[4]
+
+# http://tinyurl.com/zk52tef
+
+author = "Kafka"
+author[5]
+
+# P90
+# http://tinyurl.com/hyju2t5
+
+author = "Kafka"
+author[-1]
+
+# http://tinyurl.com/jtpx7sr
+
+author = "Kafka"
+author[-2]
+author[-3]
+
+# http://tinyurl.com/hsr831v
+
+ff = "F. Fitzgerald"
+ff = "F. Scott Fitzgerald"
+ff
+
+# P91
+# http://tinyurl.com/h4z5mlg
+
+"cat" + "in" + "hat"
+
+# http://tinyurl.com/gsrajle
+
+"cat" + " in" + " the" + " hat"
+
+# http://tinyurl.com/zvm9gng
+
+"Sawyer" * 3
