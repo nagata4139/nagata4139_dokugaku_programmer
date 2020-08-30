71e210b860918a927a7a1025171551aaaac97b61
nagata4139 <nagata4139@gmail.com>
2020-08-30 17:54:06 +0900

    更新

---
+++ Sutra_fot_Chapter5.py
@@ -0,0 +1,78 @@
+# P70
+# http://tinyurl.com/zdllght
+ "Hello".upper()
+
+# http://tinyurl.com/hfgpst5
+"Hello".replace("o", "@")
+
+# P71
+# http://tinyurl.com/h4go6kg
+
+fruit = list()
+fruit
+
+# http://tinyurl.com/jft8p7x
+
+fruit = []
+fruit
+
+# http://tinyurl.com/h2y8nos
+
+fruit = ["Apple", "Orange", "Pear"]
+fruit
+
+# P72
+# http://tinyurl.com/h9w3z2m
+
+fruit = ["Apple", "Orange", "Pear"]
+fruit.append("Banana")
+fruit.append("Peach")
+fruit
+
+# http://tinyurl.com/zhpntsr
+
+random = []
+
+random.append(True)
+random.append(100)
+random.append(1.1)
+random.append("Hello")
+random
+
+# P73
+# http://tinyurl.com/jqtlwpf
+
+fruit = ["Apple", "Orange", "Pear"]
+fruit[0]
+fruit[1]
+fruit[2]
+
+# http://tinyurl.com/za3rv95
+
+colors = ["blue", "green", "yellow"]
+colors[4]
+
+# http://tinyurl.com/h4ahvf9
+colors = ["blue", "green", "yellow"]
+colors
+colors[2] = "red"
+colors
+
+# P74
+# http://tinyurl.com/j52uvmq
+
+colors = ["blue", "green", "yellow"]
+colors
+item = colors.pop()
+item
+colors
+
+# http://tinyurl.com/jjxnk4z
+
+colors1 = ["blue", "green", "yellow"]
+colors2 = ["orange", "pink", "yellow"]
+colors1 + colors2
+
+# http://tinyurl.com/z4fnv39
+colors = ["blue","green","yellow"]
+"green" in colors
