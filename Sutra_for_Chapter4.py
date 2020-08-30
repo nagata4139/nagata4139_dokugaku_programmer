84b10597d0f040427482fb7a8f483a516be9855b
nagata4139 <nagata4139@gmail.com>
2020-08-30 16:44:11 +0900

    更新

---
+++ Sutra_for_Chapter4.py
@@ -0,0 +1,79 @@
+# P51
+# 実行しないでください
+
+f(x) = x * 2
+
+# P52
+def [関数名]([引数]):
+    [関数定義]
+
+# http://tinyurl.com//j9dctwl
+
+def f(x):
+    return x * 2
+
+# P53
+# http://tinyurl.com/zheas3d
+
+# 前のコードの続き
+
+f(2)
+
+# http://tinyurl.com/gspjcgj
+
+# 前のコードの続き
+
+result(2)
+print(result)
+
+# http://tinyurl.com/znqp8fx
+
+def f(x):
+    return x + 1
+
+z = f(4)
+
+if z == 5:
+    print("z is 5)
+else:
+    print("z is not 5")
+
+# P54
+# http://tinyurl.com/htk7tr6
+
+def f():
+    return 1 + 1
+
+result = f()
+print(result)
+
+def f(x,y,z)
+    return x + y + z
+
+result = f(1, 2, 3)
+print(result)
+
+# http://tinyurl.com/j8qyqov
+
+def f():
+    z = 1 + 1
+
+result = f()
+print(result)
+
+# P55
+# http://tinyurl.com/zfkzqw6
+
+len("Monty")
+
+# http://tinyurl.com/h75c3cf
+
+len("Python")
+
+# http://tinyurl.com/juzxg2z
+
+str(100)
+
+# http://tinyurl.com/j42qhkf
+
+int("1")
