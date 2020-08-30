a394ed2a751a0da7fe200a0438306e2e09cb5689
nagata4139 <nagata4139@gmail.com>
2020-08-30 17:35:52 +0900

    更新

--- Sutra_for_Chapter4.py
+++ Sutra_for_Chapter4.py
@@ -77,3 +77,234 @@ str(100)
 # http://tinyurl.com/j42qhkf
 
 int("1")
+
+# P56
+# http://tinyurl.com/hnk8gh2
+
+float(100)
+
+# http://tinyurl.com/jcchmlx
+
+int("110")
+int(20.54)
+
+float("16.4")
+float(99)
+
+# http://tinyurl.com/zseo21s
+
+int("Prince")
+
+# P57
+# http://tinyurl.com/zynprpg
+
+age = input("Enter your age:")
+int_age = int(age))
+if int_age < 21:
+    print("You are young!")
+else:
+    print("Wow, you are old!")
+
+# http://tinyurl.com/zhy8y4m
+def even_odd(x):
+    if x % 2 == 0:
+        print("偶数")
+    else:
+        print("奇数")
+
+even_odd(2)
+even_odd(3)
+
+# http://tinyurl.com/jk8lugl
+
+n = input("type a number:")
+n = int(n)
+if n % 2 == 0:
+    print("n is even.")
+else:
+    print("n is odd.")
+
+n = input("type a number")
+n = int(n)
+if n % 2 == 0
+    print("n is even.")
+else:
+    print("n is odd.")
+
+n = input("type a number:")
+n = int(n)
+if n % 2 == 0:
+    print("n is even.")
+else:
+    print("n is odd.")
+
+# P59
+# http://tinyurl.com/zzn22mz
+def even_odd():
+    n = input("type a number:")
+    n = int(n)
+    if n % 2 == 0:
+        print("n is even.")
+    else:
+        ptint("n is odd.")
+
+even_odd()
+even_odd()
+even_odd()
+
+# P60
+http://tinyurl.com/h4ych4h
+
+def f(x=2):
+    return x ** x
+
+print(f())
+print(f(4))
+
+# http://tinyurl.com/hm5svn9
+
+def add_it(x, y=10)
+    return x + y
+
+result = add_it(2)
+print(result)
+
+# P61
+# http://tinyurl.com/zhmxnqt
+
+x = 1
+y = 2
+z = 3
+
+# http://tinyurl.com/hgvnj4p
+
+x = 1
+y = 2
+z = 3
+
+def f():
+    print(x)
+    print(y)
+    print(z)
+
+f()
+
+# P62
+# http://tinyurl.com/znka93k
+
+def f():
+    x = 1
+    y = 2
+    z = 3
+
+print(x)
+print(y)
+print(z)
+
+f()
+
+# http://tinyurl.com/z2k3jds
+
+def f():
+    x = 1
+    y = 2
+    z = 3
+    print(x)
+    print(y)
+    print(z)
+
+f()
+
+# http://tinyurl.com/zn8zjmr
+
+if x > 100:
+    print("x is > 100")
+
+# P63
+# http://tinyurl.com/zclmda7
+
+x = 100
+
+def f():
+    global x
+    x += 1
+    print(x)
+
+f()
+
+# P64
+# http://tinyurl.com/ztpcjs4
+a = input("type a number:")
+b = input("type another")
+a = int(a)
+b = int(b)
+print(a / b)
+
+# http://tinyurl.com/ztpcjs4
+a = input("type a number:")
+b = input("type another")
+a = int(a)
+b = int(b)
+print(a / b)
+
+# P65
+# http://tinyurl.com/j2scn4f
+
+a = input("type a number:")
+b = input("type another")
+a = int(a)
+b = int(b)
+try:
+    print(a / b)
+except ZeroDivisionError:
+    print("b cannot be zero.")
+
+# P66
+a = input("type a number:")
+b = input("type another")
+a = int(a)
+b = int(b)
+try:
+    print(a / b)
+except ZeroDivisionError:
+    print("b cannot be zero.")
+
+# http://tinyurl.com/jlus42v
+try:
+    a = input("type a number:")
+    b = input("type another")
+    a = int(a)
+    b = int(b)
+    print("b cannot be zero.")
+except(ZeroDivisionError, ValueError):
+    print("Invalid input.")
+
+# P67
+# http://tinyul.com/hockur5
+
+try:
+    10 / 0
+    c = "I will never get defined"
+except ZeroDivisionError
+    print(c)
+
+# http://tinyurl.com/zhahdcg
+
+def add(x, y):
+    """
+    Return x + y
+    :param x: int.
+    :param y: int.
+    :return: int sum of x and y
+    """
+    return  x + y
+
+# P68
+# http://tinyurl.com/zptktex
+
+x = 100
+print(x)
+
+# http://tinyurl.com/hmwr4kd
+
+print(100)
