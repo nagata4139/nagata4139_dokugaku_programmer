22349006e05f2fb8de1396917babd2530c4ec43c
nagata4139 <nagata4139@gmail.com>
2020-08-31 20:59:13 +0900

    更新

---
+++ Sutra_for_Chapter7.py
@@ -0,0 +1,30 @@
+# P105
+# http://tinyurl.com/jya6kpm
+
+name = "Ted"
+for character in name:
+    print(character)
+
+# http://tinyurl.com/zeftpq8
+
+shows  = ["GOT", "Narcos", "Vice"]
+for show in shows:
+    print(show)
+
+# http://tinyurl.com/gpr5a6e
+
+coms = ("A. Development", "Friends", "Always Sunny")
+for show in coms:
+    print(show)
+
+# P106
+# http://tinyurl.com/jk7do9b
+
+people = {"G. Bluth II": "A. Development",
+          "Barney": "HIMYM",
+          "Dennis": "Always Sunny",
+          }
+
+for character in people:
+    print(character)
+
