60a897f1f7ec6de64103c4b0b24dc3dce3219f7b
nagata4139 <nagata4139@gmail.com>
2020-09-06 20:08:21 +0900

    更新

---
+++ Sutra_for_Chapter8.py
@@ -0,0 +1,47 @@
+# P119
+# http://tinyurl.com/h3ds93u
+
+import math
+
+# http://tinyurl.com/hyjo59s
+
+import math
+
+math.pow(2,3)
+
+# P120
+# http://tinyurl.com/hr3fppn
+
+# 実行結果の出力は毎回変わるだろう
+# これはランダムだから！
+
+import random
+
+random.randint(0,100)
+
+# http://tinyurl.com/jrnznoy
+
+import statistics
+
+# mean
+nums = [1, 5, 33, 12, 46, 33, 2]
+statistics.mean(nums)
+
+# median
+statistics.median(nums)
+
+# mode
+statistics.mode(nums)
+
+# P121
+# http://tinyurl.com/zjphfho
+
+import keyword
+
+keyword.iskeyword("for")
+keyword.iskeyword("football")
+
+# http://tinyurl.com/z5v9hk3
+
+def print_hello():
+    print("Hello")
