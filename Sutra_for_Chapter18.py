2224c5e1641070b207387aaf3eb9fdc00e829697
nagata4139 <nagata4139@gmail.com>
2020-09-14 22:38:56 +0900

    更新

---
+++ Sutra_for_Chapter18.py
@@ -0,0 +1,36 @@
+# P217
+# http://tinyurl.com/hmookdf
+
+# $ pip
+
+# P218
+# Ubuntu or macOS
+# http://tinyurl/comhchso7u
+
+# $ sudo pip install Flask==0.11.1
+
+# Windows(管理者権限)
+# http://tinyurl.com/hyxm3vt
+
+# $ pip install Flask==0.11.1
+
+# http://tinyur.com/h59sdyu
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello,World!"
+
+app.run(port=8000)
+
+# P219
+
+# $ pip freeze
+
+# P220
+# http://tinyurl.com/ht8mleo
+
+# $ pip uninstall Fask
