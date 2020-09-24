e57737ee35c3697b2883dec9d40c7036711f6502
nagata4139 <nagata4139@gmail.com>
2020-09-24 22:15:48 +0900

    更新

--- Sutra_for_Camp1.py
+++
@@ -1,8 +0,0 @@
-# リスト1.1 Pythonのバージョンを確認
-$ python3 -V
-Python 3.6.7
-
-# リスト1.2 Pythonのバージョンを確認
-
-$ python3 -V
-Python 3.6.5
---
+++ Sutra_for_Camp1～6.py
@@ -0,0 +1,40 @@
+# リスト1.1 Pythonのバージョンを確認
+#$ python3 -V
+#Python 3.6.7
+
+# リスト1.2 Pythonのバージョンを確認
+
+#$ python3 -V
+#Python 3.6.5
+
+# リスト2.1 Pythonインタープリタの起動
+
+#$ python3
+#Python 3.6.7 (v3.6.7:6ec5cf24b7, Oct 20 2018, 03:02:14)
+#[GCC 4.2.1 Compatible Apple LLVM 6.0 (clang-600.0.57)] on darwin
+#Type "help", "copyright", "credits" or "license" for more information.
+
+# リスト2.2 四則演算と代入
+print(2 + 2)
+
+print(3 - 8)
+
+print(6 * 9)
+
+print(8 / 2)
+
+print(5 % 2)
+
+width = 60
+height = 90
+print(width * height)
+
+# リスト2.3 文字列型
+
+print("Hello,World!")
+
+print("Monty Python's Flying Circus")
+
+# リスト2.4 リスト
+
+print(["Hello",3])
