2a551b4ed745fd528437b8300b42e3fc7f4d1fbb
nagata4139 <nagata4139@gmail.com>
2020-09-26 09:56:27 +0900

    更新

--- Sutra_for_Camp1～6.py
+++ Sutra_for_Camp1～6.py
@@ -250,3 +250,124 @@ print("pain-au-chocolat".split("-"))
 # リスト 3.17 文字列の結合
 
 print("-".join(["pain", "de", "campagne"]))
+
+# リスト 4.1 リストの定義
+
+print(["spam", "egg", 0.5])
+
+# リスト 4.2 リストの基本的な使い方
+
+print(["spam", "ham"] + ["egg"])              # リストの結合
+
+print(["spam"] * 5)                           # リストの繰り返し
+
+print(["spam", "ham", "egg"][0] )             # リストの0番目を取得する
+
+print(["spam", "ham", "egg"][1:])             # リストのスライス(1番目以降)
+
+print(len(["spam", "ham", "egg"]))            # リストの長さ
+
+print("ham" in ["spam", "ham", "egg"])        # リストに特定の文字列が含まれるか
+
+# リスト 4.3 for文とリスト
+
+for animal in ["cat", "dog", "snake"]:
+    print(animal)
+
+# リスト 4.4 リストへの要素追加
+
+animals = ["cat", "dog", "snake"]
+animals.append("elephant")
+print(animals)
+
+# リスト 4.5 一般的なfor文
+ret = []
+for animal in animals:
+    ret.append(len(animal))
+print(ret)
+
+# リスト 4.6 リスト内包表記
+
+print([len(animal) for animal in animals])
+
+# リスト 4.7 シーケンス型から複数変数への代入
+
+dog, cat = ["dog", "cat"]
+print(dog)
+print(cat)
+
+# リスト 4.8 タプルの定義
+
+print(("spam", "ham", 4))
+
+# リスト 4.9 タプルの基本的な使い方
+
+print(("spam", "ham") + ("egg",))             # タプルの結合
+
+print(("spam",) * 5)                          # タプルの繰り返し
+
+print(("spam", "ham", "egg")[0])              # タプルの0番目を取得する
+
+print(("spam", "ham", "egg")[1:])             # タプルのスライス(1番目以降)
+
+print(len(("spam", "ham", "egg")))            # タプルの長さ
+
+print("ham" in ("spam", "ham", "egg"))        # タプルに特定の文字列が含まれるか
+
+# リスト 4.10 1要素のタプル
+
+print(("spam",))
+
+print(("spam"))
+
+# リスト 4.11 括弧を省略したタプル
+
+print("dog", "cat")
+
+# リスト 4.12 タプルを返す関数
+def head_splitter(seq):
+    return seq[0], seq[1:]
+
+head, tail = head_splitter(["head", "body", "tail"])
+print(head)
+print(tail)
+
+# リスト 4.13 要素数の多いタプルを返す関数
+
+def bad_implementation():
+    return "username", "user_password", "user_id", "user_permission1", "user_permission2"
+
+username, user_password, user_id, user_permission1, user_permission2 = bad_implementation()
+
+# リスト 4.14 辞書
+
+user_info = {"user_name": "taro", "last_name": "Yamada"}
+print(user_info)
+
+# リスト 4.15 辞書からの値の取り出し
+
+print(user_info["user_name"])
+
+# リスト 4.16 辞書への値の設定
+
+user_info["first_name"] = "Taro"
+print(user_info)
+
+# リスト 4.17 辞書のin
+
+print("user_name" in user_info)
+print("bio" in user_info)
+
+# リスト 4.18 存在しないキーの参照
+
+#user_info["bio"]
+#Traceback (most recent call last):
+#  File "C:/Users/M.Nagata/PycharmProjects/pythonProject/nagata4139_dokugaku_programmer/Sutra_for_Camp1～6.py", line 363, in <module>
+#    user_info["bio"]
+#KeyError: 'bio'
+
+# リスト 4.19 存在しないキーへのget
+
+user_info.get('user_name')
+bio = user_info.get('bio')
+print(bio)
