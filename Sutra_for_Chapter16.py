da193763338809c8a5e494b3126c359c61990bbb
nagata4139 <nagata4139@gmail.com>
2020-10-11 22:02:02 +0900

    更新

---
+++ Sutra_for_Chapter16.py
@@ -0,0 +1,88 @@
+# P192
+# http://tinyurl.com/junx62n
+
+#$ echo Hello,World!
+
+# http://tinyurl.com/htoospk
+
+#$ python3
+
+# P193
+# http://tinyurl.com/jk2acua
+
+print("Hello,World!")
+
+# http://tinyurl.com/go2spbt
+
+#$ history
+
+# P194
+# http://tinyurl.com/hptsphp
+
+#$ pwd
+
+# P195
+# http://tinyurl.com/hjgz79h
+
+#$ cd /
+
+# http://tinyurl.com/j6ax35s
+
+#$ pwd
+
+# http://tinyurl.com/gw4d5yw
+
+#$ ls
+
+# P196
+# http://tinyurl.com/zavhjeq
+
+#$ cd ~
+#$ mkdir tstp
+
+# http://tinyurl.com/hneq2f6
+
+#$ ls
+
+# http://tinyurl.com/zp3nb21
+
+#$ cd tstp
+
+# http://tinyurl.com/z2gevk2
+
+#$ cd ..
+
+# P197
+# http://tinyurl.com/jkjjo6s
+
+#$ rmdir tstp
+
+# http://tinyurl.com/z32xn2n
+
+#$ ls
+
+#$ ls --author -l
+
+# P199
+# http://tinyurl.com/hfswo8t
+
+#$ touch .self_taught
+
+# http://tinyurl.com/zjne9f5
+
+#$ ls | less
+
+# P200
+
+#$ export x=100
+#$ echo $x
+
+# http://tinyurl.com/j5wjwdf
+
+#$ echo $x
+
+# P201
+
+#$ whoami
+
+#$ sudo echo Hello,World!
